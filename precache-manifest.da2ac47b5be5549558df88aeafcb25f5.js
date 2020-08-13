0e6ab3dc82153990ac03abcf048fc072c1ef86ac
vaib1343 <vaib1343@gmail.com>
2020-08-13 21:10:46 +0700

    Updates

---
+++ precache-manifest.da2ac47b5be5549558df88aeafcb25f5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2899a3e75f7cb7fe9e5303e31b3b4b8d",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "476638d1bcdfaf336464",
+    "url": "/covid/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "b98abe52154c6ab30a1c",
+    "url": "/covid/static/css/main.f3fc4546.chunk.css"
+  },
+  {
+    "revision": "476638d1bcdfaf336464",
+    "url": "/covid/static/js/2.e32b4231.chunk.js"
+  },
+  {
+    "revision": "d9cca716f9d7cc1e1335be5d76cd259e",
+    "url": "/covid/static/js/2.e32b4231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b98abe52154c6ab30a1c",
+    "url": "/covid/static/js/main.0c33f515.chunk.js"
+  },
+  {
+    "revision": "a58f414454ddc8009eac",
+    "url": "/covid/static/js/runtime-main.ff87d293.js"
+  },
+  {
+    "revision": "e7baea8f02d60c4a50fc84e385addf81",
+    "url": "/covid/static/media/death.e7baea8f.jpg"
+  },
+  {
+    "revision": "99e37d9f807a8197cf65679b372d764c",
+    "url": "/covid/static/media/home.99e37d9f.jpg"
+  },
+  {
+    "revision": "4071cfeb55794e35311c729af41d4f64",
+    "url": "/covid/static/media/infected.4071cfeb.jpg"
+  },
+  {
+    "revision": "d0fb84f79f4c2f4d9c35097a8e8a3d4a",
+    "url": "/covid/static/media/recovered.d0fb84f7.jpg"
+  }
+]);
